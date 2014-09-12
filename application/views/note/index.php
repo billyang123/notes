4cb1689af93ee951998ecb37301c035e03cf96e1
{杨斌斌}({TW003248}) <wb-yangbinbin@taobao.com>
2014-09-12 15:56:25 +0800

    pagin

--- application/views/note/index.php
+++ application/views/note/index.php
@@ -32,7 +32,7 @@
         <?php else: ?>
         <li class="uk-pagination-previous"><a href="/index.php/notes/index/<?=($currentPage-1) ?>" >上一页</a></li>
         <?php endif;?>
-        <?php if($currentPage==$total):?>
+        <?php if($currentPage==$total-1):?>
         <li class="uk-pagination-next uk-disabled"><a href="#">下一页</a></li>
         <?php else: ?>
         <li class="uk-pagination-next"><a href="/index.php/notes/index/<?=($currentPage+1) ?>" >下一页</a></li>
