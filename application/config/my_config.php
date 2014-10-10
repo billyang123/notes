f11ec9172fdbf17a4ca16fbaeb8b9c6abef548f5
{杨斌斌}({TW003248}) <wb-yangbinbin@taobao.com>
2014-10-10 14:49:13 +0800

    添加本地配置

--- application/config/my_config.php
+++ application/config/my_config.php
@@ -1,6 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 	
-	$config['bucket']	= 'ybbtest';
+	//$config['bucket']	= 'ybbtest';
+	$config['bucket']	= 'ybbcdn';
 	$config['accessKey'] = 'hZB4el2zIFDT7eb5XXX7qxQso3-x1s5GCLpzNmNQ';
 	$config['secretKey'] = '-5P_lb891K-xXUSNj2pTxSNK21FxnbXKvAn9dDF7';
 	$config['tmpDir'] = '/var/tmp';
