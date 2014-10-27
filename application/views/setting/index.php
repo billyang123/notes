76828c2d286ee95a2937d80100bdf2c9c0c3c6b5
{杨斌斌}({TW003248}) <wb-yangbinbin@taobao.com>
2014-10-27 18:04:48 +0800

    update

--- application/views/setting/index.php
+++ application/views/setting/index.php
@@ -13,7 +13,7 @@
 					<a href="#my-avatar-modal" class="uk-button uk-button-primary" data-uk-modal>选择</a>
 					<div class="avatar-view" id="avatar-view" title="Change the avatar">
 						<input value="<?=$userInfo['avatar']?$userInfo['avatar']:'' ?>" name="avatar" type="hidden">
-				      <?=$userInfo['avatar']?'<img src="'.$userInfo['avatar'].'" alt="Avatar">':'' ?>
+				      	<img src="<?=$userInfo['avatar']?$userInfo['avatar']:'/identicon.php?uid='.$com_user['userName'].'&size=120' ?>">
 				    </div>
 		        </div>
 			</div>
